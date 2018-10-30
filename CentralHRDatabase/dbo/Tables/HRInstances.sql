949e2619e3b5f1eccfd699709da3823e34404725
ashley wardell <ashw1984@hotmail.co.uk>
2018-10-30 18:47:08 +0000

    added database project

---
+++ CentralHRDatabase/dbo/Tables/HRInstances.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[HRInstances] (
+    [InstanceId]     INT              IDENTITY (1, 1) NOT NULL,
+    [InstanceName]   VARCHAR (255)    NOT NULL,
+    [LicenseGuid]    UNIQUEIDENTIFIER NOT NULL,
+    [LastRunSSUGuid] UNIQUEIDENTIFIER NOT NULL,
+    [VersionMajor]   INT              NOT NULL,
+    [VersionMinor]   INT              NOT NULL,
+    [Build]          INT              NOT NULL,
+    [Revision]       INT              NOT NULL,
+    [LastPing]       DATETIME         NOT NULL
+);
+
