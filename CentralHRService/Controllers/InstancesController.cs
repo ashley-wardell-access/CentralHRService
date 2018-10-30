f4fbe5fe07bd73e992c9c745e218a45f284911e7
ashley wardell <ashw1984@hotmail.co.uk>
2018-10-30 19:18:02 +0000

    Added method to hit the db

--- CentralHRService/Controllers/InstancesController.cs
+++ CentralHRService/Controllers/InstancesController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using CentralHRService.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,48 @@ namespace CentralHRService.Controllers
     [ApiController]
     public class InstancesController : ControllerBase
     {
+        [HttpPost]
+        public void InstancePing(InstanceViewModel instanceViewModel) {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                String query = "INSERT INTO [dbo].[HRInstances]"+
+                        "([InstanceName]"+
+                           ",[LicenseGuid]"+
+                           ",[LastRunSSUGuid]"+
+                           ",[VersionMajor]"+
+                           ",[VersionMinor]"+
+                           ",[Build]"+
+                           ",[Revision]"+
+                           ",[LastPing])"+
+                     "VALUES"+
+                           "(@InstanceName"+
+                           ", @LicenseGuid"+
+                           ", @LastRunSSUGuid"+
+                           ", @VersionMajor"+
+                           ", @VersionMinor"+
+                           ", @Build"+
+                           ", @Revision"+
+                           ", @LastPing)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@InstanceName", instanceViewModel.InstanceName);
+                    command.Parameters.AddWithValue("@LicenseGuid", instanceViewModel.LicenseGuid);
+                    command.Parameters.AddWithValue("@LastRunSSUGuid", instanceViewModel.LastRunSSUGuid);
+                    command.Parameters.AddWithValue("@VersionMajor", instanceViewModel.VersionMajor);
+                    command.Parameters.AddWithValue("@VersionMinor", instanceViewModel.VersionMinor);
+                    command.Parameters.AddWithValue("@Build", instanceViewModel.Build);
+                    command.Parameters.AddWithValue("@Revision", instanceViewModel.Revision);
+                    command.Parameters.AddWithValue("@LastPing", instanceViewModel.LastPing);
+
+                    connection.Open();
+                    int result = command.ExecuteNonQuery();
+
+                    // Check Error
+                    if (result < 0)
+                        Console.WriteLine("Error inserting data into Database!");
+                }
+            }
+        }
     }
 }
---
+++ CentralHRService/Models/InstanceViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CentralHRService.Models
+{
+    public class InstanceViewModel
+    {
+        public string InstanceName { get; set; }
+        public Guid LicenseGuid { get; set; }
+        public Guid LastRunSSUGuid { get; set; }
+        public int VersionMajor { get; set; }
+        public int VersionMinor { get; set; }
+        public int Build { get; set; }
+        public int Revision { get; set; }
+        public DateTime LastPing { get; set; }
+    }
+}
